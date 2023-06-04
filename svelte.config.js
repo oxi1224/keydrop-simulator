7166f9b6c0b60876a495fe99f8c297a1ad506f30
ox-i <oxi1224@o2.pl>
2023-06-04 15:13:13 +0200

    revert changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'svelte-adapter-static-digitalocean';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
