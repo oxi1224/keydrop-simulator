3ded9dc843184bbb574c038a157342a648513998
ox-i <oxi1224@o2.pl>
2023-06-04 19:45:22 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,13 @@ const config: UserConfig = {
   plugins: [sveltekit()],
   resolve: {
     alias: {
-      src: path.resolve('./src'), 
       $components: path.resolve('src/components'),
       $static: path.resolve('static'),
       $assets: path.resolve('src/assets'),
       '$lib/server': path.resolve('src/lib/server')
     },
-    preserveSymlinks: false
+    preserveSymlinks: true
   },
-  server: {
-    fs: {
-      allow: ['..']
-    }
-  }
 };
 
 export default config;
