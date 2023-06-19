4b506311ccf433262cc59cfe343eec768558adc5
ox-i <oxi1224@o2.pl>
2023-06-19 12:10:48 +0200

    start timer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
   import CaseSection from '$components/case/CaseSection.svelte';
-  import type { CaseSection as CaseSectionType } from '$lib';
+  import { TimeInMs, type CaseSection as CaseSectionType } from '$lib';
   export let data: { sections: CaseSectionType[] };
 
-  // let time = '';
-  // const duration = 1686329595441 + TimeInMs.Minute * 90;
-  // setInterval(() => {
-  //   const diff = duration - new Date().getTime();
+  let time = '';
+  const duration = 1687169438758 + TimeInMs.Minute * 90;
+  setInterval(() => {
+    const diff = duration - new Date().getTime();
 
-  //   const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  //   const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  //   const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-  //   time = hours + ":" + minutes + ":" + seconds;
+    time = hours + ":" + minutes + ":" + seconds;
 
-  //   if (diff < 0) time = 'NIEDLUGO';
-  // }, 1000);
+    if (diff < 0) time = 'NIEDLUGO';
+  }, 1000);
 </script>
 
 <main class="bg-no-repeat" style="background-position: center top; background-size: auto 1400px;">
@@ -30,7 +30,7 @@
               <div class="text-red text-lg text-center">
                 SKRZYNKI SĄ AKTUALIZOWANE, PROSZE CZEKAĆ...
                 <br />
-                <!-- Oczekiwany czas: {time} -->
+                Oczekiwany czas: {time}
               </div>
             {/if}
           {/each}
