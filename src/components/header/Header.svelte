af8ff2f44f4a2ee4019d002726ad333221123069
ox-i <oxi1224@o2.pl>
2023-07-24 22:09:14 +0200

    add back header to mobile

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -50,7 +50,7 @@
 
 <header class="sticky top-0 z-40 md:relative bg-navy-600 select-none">
   <button
-    class="hidden is-open w-full bg-navy-800 text-navy-200 text-center py-2 md:is-open:block cursor-pointer"
+    class="is-open w-full bg-navy-800 text-navy-200 text-center py-2 is-open:block cursor-pointer"
     on:click="{(e) => e.currentTarget.classList.remove('is-open')}"
   >
     <h1>
