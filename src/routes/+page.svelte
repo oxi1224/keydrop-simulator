8bd0b7083da004b53d308fc140f5be0cd1dd4f0c
ox-i <oxi1224@o2.pl>
2023-10-15 13:00:07 +0200

    Well, I guess that's it v2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 
 <main class="w-screen h-screen">
   <h1 class="text-red text-[7.5rem] text-center">STRONA ZAMKNIĘTA NA ŻYCZENIE KEYDROPA.</h1>
-  <h2 class="text-white text-[2.5rem] text-center">Strona i tak wytrzymała dłużej niż się spodziewałem, lecz niestety to koniec przygody z keydrop-simulator</h2>
+  <h2 class="text-white text-[2.5rem] text-center">
+    Strona i tak wytrzymała dłużej niż się spodziewałem, lecz niestety to koniec przygody z keydrop-simulator<br>
+    Więcej informacji na <a href="https://discord.gg/FnGrsSgNTS" class="underline text-blue-500 hover:text-blue-900">discordzie</a>
+  </h2>
 </main>
